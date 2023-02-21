350404a5e4ece7921e7ae5e00f9d1ee87e0dffd6
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-02-21 16:53:32 +0530

    removed unnessary files

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 // connect to mongodb atlas
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => {
-	console.log('CONNECTED TO MONGODB ATLAS');
-});
+mongoose
+	.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+	.then(() => {
+		console.log('CONNECTED TO MONGODB ATLAS');
+	})
+	.catch((error) => {
+		console.log('something wrong happened');
+	});
 
 app.listen(PORT, () => {
 	console.log('Server Started at PORT ', PORT);
