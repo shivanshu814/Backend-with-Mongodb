86f3c7ab31055bba588615a9bf7e12b134ab574f
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-02-21 16:47:49 +0530

    environment variables

--- index.js
+++ index.js
@@ -3,18 +3,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 // connect to mongodb atlas
-mongoose
-	.connect(
-		'mongodb+srv://shivanshu:shivanshu@cluster0.3jx18xk.mongodb.net/?retryWrites=true&w=majority',
-		{ useNewUrlParser: true }
-	)
-	.then(() => {
-		console.log('CONNECTED TO MONGODB ATLAS');
-	});
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => {
+	console.log('CONNECTED TO MONGODB ATLAS');
+});
 
 app.listen(PORT, () => {
 	console.log('Server Started at PORT ', PORT);
